fdcfa82d86430bd4e0b901087a07b8e7e1d054bf
donell <donell.wambua@student.moringaschool.com>
2023-06-22 15:59:15 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  <form id="create-task-form" action="#" method="POST">
-      <label for="new-task-description">Task description:</label>
-      <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
-      <input type="submit" value="Create New Task">
-    </form> 
+
+})
